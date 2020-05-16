5ae33d4f8ce27995657d72c53188ee793c4a4c22
shubham <shubhamshirapuri10@gmail.com>
2020-05-16 19:01:49 +0530

    first day

---
+++ models/user.js
@@ -0,0 +1,39 @@
+var mongoose = require("mongoose");
+var userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    lastname:{
+        type: String,
+        maxlength: 32,
+        trim: true
+    },
+    userinfo:{
+        type: String,
+        trim: true
+    },
+    email:{
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    password:{
+        type: String,
+        trim: true
+    },
+    salt: String,
+    role:{
+        type: Number,
+        default: 0
+    },
+    purchases:{
+        type: Array,
+        default: []
+    }
+});
+
+module.exports = mongoose.model("User", userSchema)
