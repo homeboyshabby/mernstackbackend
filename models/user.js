5a8817ff9143b24943768c397166af2bc2c9b3b2
shubham <shubhamshirapuri10@gmail.com>
2020-05-16 21:13:39 +0530

    first day code

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 var mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require('uuid/v1');
+
 var userSchema = new mongoose.Schema({
     name:{
         type: String,
@@ -21,9 +24,9 @@ var userSchema = new mongoose.Schema({
         unique: true,
         required: true
     },
-    password:{
+    encry_password:{
         type: String,
-        trim: true
+        required: true
     },
     salt: String,
     role:{
@@ -34,6 +37,33 @@ var userSchema = new mongoose.Schema({
         type: Array,
         default: []
     }
-});
+}, {timestamps: true});
+
+userSchema.virtual("password")
+.set(function(password){
+    this._password = password
+    this.salt = uuidv1();
+    this.encry_password = this.securePassword(password);
+})
+
+.get(function(){
+    return this._password
+})
+
+userSchema.method = {
+    authenticate: function(plainpassword){
+        return this.securePassword(plainpassword) === this.encry_password
+    },
+    securePassword: function(plainpassword){
+        if(!password) return "";
+        try {
+            return crypto.createHmac('sha256', this.salt)
+            .update(plainpassword)
+            .digest('hex');
+        } catch (error) {
+            return "";
+        }
+    }
+}
 
 module.exports = mongoose.model("User", userSchema)
